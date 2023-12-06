ec205366197dd202fe019c01ccbed382686ff351
Vitor Sousa Mesquita <vitorsmfff@yahoo.com>
2023-12-06 16:47:01 -0300

    ecommerce-store

---
+++ app/(routes)/cart/page.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import Container from "@/components/ui/container";
+import useCart from "@/hooks/use-cart";
+import CartItem from "./components/cart-item";
+import Summary from "./components/summary";
+import { useEffect, useState } from "react";
+
+const CartPage = () => {
+
+    const [isMounted, setIsMounted] = useState(false)
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    const cart = useCart();
+
+    return (
+        <div className="bg-white">
+            <Container>
+                <div className="px-4 py-16 sm:px-6 lg:px-8">
+                    <h1 className="text-3xl font-bold text-black">Carrinho de Compras</h1>
+                    <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+                        <div className="lg:col-span-7">
+                            {cart.items.length === 0 && <p className="text-neutral-500">Sem itens no carrinho.</p>}
+                            <ul>
+                                {cart.items.map((item) => (
+                                    <CartItem
+                                        key={item.id}
+                                        data={item}
+                                    />
+                                ))}
+                            </ul>
+                        </div>
+                        <Summary />
+                    </div>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default CartPage;
