ae6ceaecf68aac209c6cc63dbd5489d4f7e49358
Vitor Sousa Mesquita <vitorsmfff@yahoo.com>
2023-12-11 12:59:47 -0300

    s

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="border-t">
+    <footer className="bg-black border-t">
         <div className="mx-auto py-10">
-            <p className="text-center text-xs text-black">
+            <p className="text-center text-xs text-white">
                 &copy; 2023 Fetiche Boutique Erótica, Inc. All rights reserved.
             </p>
         </div>
