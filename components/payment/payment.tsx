73af2796431f1fed92e1c576f57bc96dad86706e
Vitor Sousa Mesquita <vitorsmfff@yahoo.com>
2023-12-24 17:32:22 -0300

    kopy

--- components/payment/payment.tsx
+++ components/payment/payment.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect } from 'react';
 import { initMercadoPago, Wallet } from '@mercadopago/sdk-react';
+import { useRouter } from 'next/navigation';
 
 interface PaymentProps {
   preferenceId: string;
@@ -11,13 +12,23 @@ const Payment: React.FC<PaymentProps> = ({
   preferenceId
 }) => {
 
+    const router = useRouter()
+
+    const redirectSuccess = () => {
+      router.push(`/cart/155fde0b-a678-4d94-9c9c-936d71c925ee/success`)
+    }
+
     useEffect(() => {
       initMercadoPago(process.env.NEXT_PUBLIC_MERCADO_PAGO_SAMPLE_PUBLIC_KEY!, { locale: 'pt-BR' });
     }, []);
 
     return (
       <div>
-        <Wallet initialization={{preferenceId: preferenceId as string, redirectMode: 'modal'}}/>
+        <Wallet 
+        initialization={{preferenceId: preferenceId, redirectMode: "blank"}}
+        onReady={() => redirectSuccess()}
+        onError={() => {}}
+        />
       </div>
     );
 };
